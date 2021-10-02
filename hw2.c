1d4beed3db7fcd17c078a3d7136ba96087839c63
YeongJoon <60257302+Soosos@users.noreply.github.com>
2021-10-03 02:12:30 +0900

    Update hw2.c

--- hw2.c
+++ hw2.c
@@ -1,1 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
 
+#include <stdio.h>
+
+int main(void) {
+	double km = 0.0;
+	int a = 0; //scanf return value 
+
+	printf("Please enter kilometers: ");
+	a = scanf("%lf", &km);
+	double mile = km / 1.609;
+
+	printf("%.1lf km is equal to %.1lf miles.", km, mile);
+
+	return 0;
+}
