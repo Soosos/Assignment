6622faf2ebd5bde098670cf2a6174ede4c7cf0d6
201910937@sangmyung.kr <201910937@sangmyung.kr>
2021-11-28 01:44:00 +0900

    hw8

---
+++ hw8.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+int main() {
+	int ch;
+
+	while (1) {
+		ch = getchar();
+		if (ch == '\n') {
+			break;
+		}
+		else {
+			putchar(convCase(ch));
+		}
+	}
+
+	return 0;
+}
+
+int convCase(int ch)
+{
+	const int diff = 'a' - 'A';
+	if (ch >= 'A' && ch <= 'Z')
+		return ch + diff;
+	else if (ch >= 'a' && ch <= 'z')
+		return ch - diff;
+	else
+		return ch;
+}
+
